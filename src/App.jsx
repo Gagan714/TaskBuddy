7b2951b7105a71365533c40e08f35b18576b5e77
Gagan714 <gaganursmg@gmail.com>
2024-04-17 17:50:11 +0530

    Small changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ function App() {
     <div>
       <Navbar/>
       <div className="container flex justify-center items-center py-3">
-        <div className="box h-[85vh] w-1/2 bg-slate-300 rounded-xl">
+        <div className="box h-[85vh] w-1/2 bg-slate-200 rounded-xl">
           <div className="font-bold px-28 py-1 text-2xl">TaskBuddy - Manage Your Tasks At One Place</div>
-          <div className="font-bold px-2 py-1 text-xl">Add a Todo Task</div>
+          <div className="font-bold px-2 py-2 text-xl">Add a Todo Task</div>
+          <div className="flex justify-center items-center">
+          <input className="w-4/5 outline-none" type="text" />
+            <button className="border border-purple-600 px-3 rounded-2xl ml-2 text-white bg-purple-700">Save</button>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const Navbar = () => {
   return (
-    <nav className="flex h-12 w-full text-white bg-blue-900 items-center justify-between px-16">
+    <nav className="flex h-12 w-full text-white bg-blue-900 items-center justify-between px-48">
       <span className="logo font-bold text-xl">TaskBuddy</span>
         <ul className="flex gap-5">
             <li className="cursor-pointer hover:font-semibold">Home</li>
